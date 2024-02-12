e8d68b1c70db2a23bead0a4835ec5e2d5ebe5d1c
Vladislav Holdovanskiy <89700552+BattleToady@users.noreply.github.com>
2024-02-12 20:02:27 +0200

    Setting main menu infrastructure

--- iganta.py
+++ iganta.py
@@ -1,10 +1,20 @@
 from kivy.app import App
 from kivy.uix.screenmanager import ScreenManager, Screen		
+from kivy.lang import Builder
+from MenuScreen import MenuScreen
+from screens.DailyRoutineScreen import DailyRoutineScreen
+from screens.StrategicGoalsScreen import StrategicGoalsScreen
 
+Builder.load_file('MenuScreen.kv') 
+Builder.load_file('.\screens\DailyRoutineScreen.kv') 
+Builder.load_file('.\screens\StrategicGoalsScreen.kv') 
 
 class igantaApp(App):
 	def build(self):
 		sm = ScreenManager()
+		sm.add_widget(MenuScreen(name = 'MenuScreen'))
+		sm.add_widget(DailyRoutineScreen(name = 'DailyRoutineScreen'))
+		sm.add_widget(StrategicGoalsScreen(name = 'StrategicGoalsScreen'))
 		return sm
 
 if __name__ == '__main__':
